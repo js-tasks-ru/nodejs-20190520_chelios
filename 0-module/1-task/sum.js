76c3a6bcc41ef2ba3b745bac7abcfaacd1f82582
Nikolay Stoma <berlioz@iMac-Nikolay.local>
2019-05-14 19:24:13 +0300

    add solution for 0-module 1-task

--- 0-module/1-task/sum.js
+++ 0-module/1-task/sum.js
@@ -1,5 +1,9 @@
 function sum(a, b) {
-  /* ваш код */
+  if (typeof a == 'number' && typeof b == 'number') {
+    return a + b;
+  } else {
+    throw new TypeError('Один или несколько аргументов - не число.');
+  }
 }
 
 module.exports = sum;
